b3e5e188db1fcdb37dd1ab698924d1a19a459f0a
Heaveline-Helios <96892125+Heaveline-Helios@users.noreply.github.com>
2022-07-14 15:03:10 -0400

    Remade script and added executable permission

--- docker.sh
+++ docker.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-#setting up the repository
-sudo apt update
-sudo apt install ca-certificates curl gnupg lsb-release
-#adding dockers gpg key
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#updating repository and installing docker engine
-sudo apt update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt update
+apt install ca-certificates curl gnupg lsb-release
+sleep 1
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sleep 1
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sleep 1
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sleep 1
+docker run hello-world
+echo "Docker is installed!"
