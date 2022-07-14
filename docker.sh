e3d483665672cd8fd31877a275b754c8b764ba7d
Heaveline-Helios <96892125+Heaveline-Helios@users.noreply.github.com>
2022-07-14 15:11:00 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,14 +3,14 @@ apt update
 apt install ca-certificates curl gnupg lsb-release
 sleep 1
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sleep 1
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 sleep 1
 apt update
-apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sleep 1
 docker run hello-world
 echo "Docker is installed!"
